46e809169d759b46db8c25cec110e636db180d86
Nick Barry <itsnickbarry@protonmail.ch>
2021-09-13 13:13:43 -0500

    fix optional disambiguatePaths option

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ declare module 'hardhat/types/config' {
   interface HardhatUserConfig {
     contractSizer?: {
       alphaSort?: boolean,
-      disambiguatePaths: boolean,
+      disambiguatePaths?: boolean,
       runOnCompile?: boolean,
       strict?: boolean,
     }
