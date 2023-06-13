f2e9592786bf439124f452a9a6a76a2c354fac12
Nick Barry <itsnickbarry@protonmail.ch>
2023-06-13 22:30:11 +0300

    link to relevant EIPs inline

--- tasks/size_contracts.js
+++ tasks/size_contracts.js
@@ -8,8 +8,12 @@ const {
   TASK_COMPILE,
 } = require('hardhat/builtin-tasks/task-names');
 
+// see EIPs 170 and 3860 for more information
+// https://eips.ethereum.org/EIPS/eip-170
+// https://eips.ethereum.org/EIPS/eip-3860
 const DEPLOYED_SIZE_LIMIT = 24576;
 const INIT_SIZE_LIMIT = 49152;
+
 const UNITS = { 'B': 1, 'kB': 1000, 'KiB': 1024 };
 
 task(
